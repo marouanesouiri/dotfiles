69baa7a96896493ce6646a5b7fbea05be9d2921c
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-26 18:43:26 +0000

    edit: remove gaps when only one window is open

--- awesome/.config/awesome/tags.lua
+++ awesome/.config/awesome/tags.lua
@@ -8,7 +8,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = true, -- Make this tag the selected one on startup
-	gap_single_client = true,
+	gap_single_client = false,
 	-- master_width_factor = 0.6,
 })
 
@@ -19,18 +19,18 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true,
+	gap_single_client = false,
 	-- master_width_factor = 0.6,
 })
 
 awful.tag.add(_, {
-	name = "3",
-	id = "3",
+	name = "3", -- The tag name
+	id = "3", -- The tag id
 	screen = screen[1], -- Assign the tag to the first screen
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true,
+	gap_single_client = false,
 	-- master_width_factor = 0.6,
 })
 
@@ -41,7 +41,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true, -- Set to false if you want to remove gaps if only one window is opened
+	gap_single_client = false, -- Set to false if you want to remove gaps if only one window is opened
 	-- master_width_factor = 0.6,
 })
 
@@ -52,7 +52,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true, -- Set to false if you want to remove gaps if only one window is opened
+	gap_single_client = fals, -- Set to false if you want to remove gaps if only one window is opened
 	-- master_width_factor = 0.6,
 })
 
@@ -63,7 +63,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true, -- Set to false if you want to remove gaps if only one window is opened
+	gap_single_client = false, -- Set to false if you want to remove gaps if only one window is opened
 	-- master_width_factor = 0.6,
 })
 
@@ -74,7 +74,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true, -- Set to false if you want to remove gaps if only one window is opened
+	gap_single_client = false, -- Set to false if you want to remove gaps if only one window is opened
 	-- master_width_factor = 0.6,
 })
 
@@ -85,7 +85,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true, -- Set to false if you want to remove gaps if only one window is opened
+	gap_single_client = false, -- Set to false if you want to remove gaps if only one window is opened
 	-- master_width_factor = 0.6,
 })
 
@@ -96,7 +96,7 @@ awful.tag.add(_, {
 	layout = awful.layout.suit.tile, -- Set a specific layout for the tag
 	gap = 2, -- Set gaps between clients in the tag
 	selected = false, -- Make this tag the selected one on startup
-	gap_single_client = true, -- Set to false if you want to remove gaps if only one window is opened
+	gap_single_client = false, -- Set to false if you want to remove gaps if only one window is opened
 	-- master_width_factor = 0.6,
 })
 
@@ -106,6 +106,6 @@ awful.tag.add("", {
 	screen = awful.screen.focused(),
 	layout = awful.layout.suit.tile,
 	selected = false,
-	gap_single_client = true,
+	gap_single_client = false,
 	hidden = true, -- This hides the tag from the workspace widget
 })
