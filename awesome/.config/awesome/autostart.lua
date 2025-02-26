26676eb302944019013d2d78714288979c2e1608
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-26 18:33:59 +0000

    edit: auto start emacs deamon and udiskie

--- awesome/.config/awesome/autostart.lua
+++ awesome/.config/awesome/autostart.lua
@@ -4,6 +4,9 @@ local awful = require("awful")
 -- once
 awful.spawn.once("nm-applet")
 awful.spawn.once("picom")
+awful.spawn.once("udiskie")
+awful.spawn.once("/usr/bin/emacs --daemon")
+-- awful.spawn.once("xcompmgr -c -C -f -n -I 0.1 -O 0.1 -D 5")
 
 -- spawn
 -- awful.spawn.with_shell("notify-send hello")
