8285ef0511e6cdf3025c31c54f2be0b346cf68e8
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-03-08 23:11:08 +0000

    feat(autostarts): start picom on login

--- awesome/.config/awesome/autostart.lua
+++ awesome/.config/awesome/autostart.lua
@@ -3,7 +3,7 @@ local awful = require("awful")
 
 -- once
 awful.spawn.once("nm-applet")
-awful.spawn.once("picom")
+awful.spawn.once("picom --backend glx --blur-method dual_kawase --blur-size 10")
 awful.spawn.once("udiskie")
 awful.spawn.once("/usr/bin/emacs --daemon")
 -- awful.spawn.once("xcompmgr -c -C -f -n -I 0.1 -O 0.1 -D 5")
